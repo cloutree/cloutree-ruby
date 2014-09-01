c07339048658e2657d271648b3a89cc13dba2d05
Ivan Shamatov <status.enable@gmail.com>
2014-09-01 17:17:11 +0400

    trying to run

--- lib/cloutree.rb
+++ lib/cloutree.rb
@@ -22,26 +22,42 @@ module Cloutree
     end
 
     def upload(file)
-      request = new_request
-
-      request.headers["HTTP_API_KEY"] = app_key
-      
       filename = File.basename(file)
-      request.headers["HTTP_FILENAME"] = filename
-      
       time = Time.now.to_i
-      request.headers["HTTP_TIMESTAMP"] = time
-      
-      string_to_sha = [app_key, time, filename, app_secret].join(":")
-      request.headers["HTTP_CHECKSUM"] = Digest::SHA1.hexdigest(string_to_sha)
-      
-      request.body = File.read(file)
+      string_to_sha = [app_key, time, filename, secret].join(":")
+      checksum = Digest::SHA1.hexdigest(string_to_sha)
 
-      HTTPI.post(request, :curb)
-    end
+      command = "curl 'https://cloutr.ee/upload' "
+      command += "--data-binary '@#{file}'"
+      command += "-H 'KEY: #{app_key}' "
+      command += "-H 'CHECKSUM: #{}'"
+      command += "-H 'FILENAME: #{}'"
+      command += "-H 'TIMESTAMP: #{}'"
+      `command`
+    end 
+
+    # def upload_old(file)
+    #   request = new_request
+
+    #   request.headers["HTTP_KEY"] = app_key
+      
+    #   request.headers["HTTP_FILENAME"] = filename
+      
+    #   time = Time.now.to_i
+    #   request.headers["HTTP_TIMESTAMP"] = time
+      
+    #   string_to_sha = [app_key, time, filename, secret].join(":")
+    #   request.headers["HTTP_CHECKSUM"] = Digest::SHA1.hexdigest(string_to_sha)
+      
+    #   request.auth.ssl.verify = :none
+
+    #   request.body = File.read(file)
+
+    #   HTTPI.post(request, :curb)
+    # end
 
     def new_request
-      HTTPI::Request.new("http://cloutr.ee/upload")
+      HTTPI::Request.new("https://cloutr.ee/upload")
     end
   end
 end
