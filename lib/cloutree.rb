97a893f2137a343e1bf94695898216dbeb101258
Ivan Shamatov <status.enable@gmail.com>
2014-09-08 23:34:20 +0400

    curl command

--- lib/cloutree.rb
+++ lib/cloutree.rb
@@ -1,7 +1,7 @@
 require 'digest/sha1'
 require 'singleton'
 require 'json'
-require 'curb'
+# require 'curb'
 
 require_relative "cloutree/version"
 
@@ -43,14 +43,21 @@ module Cloutree
       string_to_sha = [app_key, time, filename, app_secret].join(":")
       checksum = Digest::SHA1.hexdigest(string_to_sha)
 
-      c = Curl::Easy.new("https://cloutr.ee/upload")
-      c.ssl_verify_peer = false
-      c.headers["KEY"] = "#{app_key}"
-      c.headers["CHECKSUM"] = "#{checksum}"
-      c.headers["FILENAME"] = "#{filename}"
-      c.headers["TIMESTAMP"] = "#{time}"
-      c.http_post(File.read(file))
-      @result = JSON.parse(c.body_str)
+      # c = Curl::Easy.new("https://cloutr.ee/upload")
+      # c.ssl_verify_peer = false
+      # c.headers["KEY"] = "#{app_key}"
+      # c.headers["CHECKSUM"] = "#{checksum}"
+      # c.headers["FILENAME"] = "#{filename}"
+      # c.headers["TIMESTAMP"] = "#{time}"
+      # c.http_post(File.read(file))
+      command = "curl 'https://cloutr.ee/upload' "
+      command += "--data-binary '@#{file}'"
+      command += "-H 'KEY: #{app_key}' "
+      command += "-H 'CHECKSUM: #{}'"
+      command += "-H 'FILENAME: #{}'"
+      command += "-H 'TIMESTAMP: #{}'"
+      res = `command`
+      @result = JSON.parse(res)
       @result["success"]
     end 
 
